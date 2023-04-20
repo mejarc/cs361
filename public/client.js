e8f916a4214e1f8046f963d611d34cec9dcf536c
Melanie Archer <job@twobanjos.com>
2023-04-20 12:07:21 -0700

    Show/hide content
    * screens are now sections in one article
    * delete JS not in use
    * add event listener to "get photos" button
    
    TODO: add event listener to other buttons

--- public/client.js
+++ public/client.js
@@ -1,7 +1,5 @@
-
 "use strict";
 const key = "NNu9H9hYedeITLk_PuCxWDs5R7B41fXawA-pURd-yYE";
-
 
 // Detect user's location on window load
 // Adapted from:
@@ -73,42 +71,69 @@ Date: April 2023
 Adapted by Melanie Archer
 */
 
-async function getPhotos(locale) {
-  const UNSPLASH = `https://api.unsplash.com/photos?client_id=${key}&order_by=latest&location=${locale}`;
+async function getPhotos(place) {
+  const UNSPLASH = `https://api.unsplash.com/photos?client_id=${key}&order_by=latest`;
+
+  // TODO you must take the photo id of each photo and then get its location in a separate call
   fetch(UNSPLASH)
     .then((resp) => resp.json())
     .then((json) => {
       if (json.error) {
-        // TODO: add text content
+        // TODO: add text content for user
         showError(json.error);
       } else {
-        console.log(`using ${locale}`);
+        console.log(`using ${place}`);
         usePhotos(json);
       }
     });
 }
+
+// Remove any images already placed
+const emptyPhotoGallery = (gallery) => {
+  while (gallery.lastChild) {
+    gallery.removeChild(gallery.lastChild);
+  }
+};
+
 const usePhotos = (data) => {
-  const cards = document.querySelector('#gallery .cards');
-  for (let i = 0; i < data.length; i++){
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    const img = document.createElement('img');
-    const p = document.createElement('p');
-    li.className = 'card';
-    a.setAttribute('href', data[i].links.html);
-    img.setAttribute('src', data[i].urls.thumb);
-    img.setAttribute('alt', data[i].alt_description);
+  const cards = document.querySelector("#gallery .cards");
+  emptyPhotoGallery(cards);
+
+  for (let i = 0; i < data.length; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    li.className = "card";
+    a.setAttribute("href", data[i].links.html);
+    img.setAttribute("src", data[i].urls.thumb);
+    img.setAttribute("alt", data[i].alt_description);
     a.appendChild(img);
     li.appendChild(a);
     cards.appendChild(li);
-
+    // Show gallery screen
+    showNextScreen("#screen2");
   }
 };
+
+const showNextScreen = (elm) => {
+  const trigger = document.querySelector(elm);
+  trigger.classList.toggle("active");
+
+  trigger.nextElementSibling.classList.toggle("inactive");
+  trigger.nextElementSibling.className = "active";
+
+  trigger.className = "inactive";
+};
+
 // Event listeners
 document.addEventListener("DOMContentLoaded", (evt) => {
   let locale = findUserPlace();
   addPlace(locale);
-  
-const fetchPhotos = document.getElementById("fetchPhotos");
-fetchPhotos.addEventListener("click", getPhotos(locale));
+
+  const fetchPhotos = document.getElementById("fetchPhotos");
+  fetchPhotos.addEventListener("click", () => {
+    showNextScreen("#screen1");
+    getPhotos(locale.locality);
+  });
 });
