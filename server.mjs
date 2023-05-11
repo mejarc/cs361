cf646ec8fde34c0c6c6dacd3921e3356d5666236
Melanie Archer <job@twobanjos.com>
2023-05-11 16:29:55 -0700

    Connect to zip code microservice: add CORS package; revise zip fetching JS

--- server.mjs
+++ server.mjs
@@ -2,10 +2,11 @@ import "dotenv/config";
 import express from "express";
 import asyncHandler from "express-async-handler";
 import fetch from "node-fetch";
+import cors from 'cors';
 
 const PORT = 3467;
 const app = express();
-
+app.use(cors());
 app.use(express.static("public"));
 
 // Respond using an error handler middleware function when it doesn't work.
